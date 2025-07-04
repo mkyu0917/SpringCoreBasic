98d48a4929f4417a6775e956a110963bb481f4e2
mkyu0917 <mkyu0917@gmail.com>
2025-07-04 15:14:14 +0900

    주문 로직 구현

--- src/main/java/com/hello/core/member/MemoryMemberReposiotry.java
+++ src/main/java/com/hello/core/member/MemoryMemberReposiotry.java
@@ -17,4 +17,17 @@ public class MemoryMemberReposiotry implements MemberRepository{
     public Member findById(Long memberId) {
         return store.get(memberId);
     }
+
+    public static class MemeberApp {
+
+        public static void main(String[] args) {
+            MemberService memberService = new MemberServiceImpl();
+            Member member = new Member(1L,"memberA", Grade.VIP);
+            memberService.join(member);
+
+            Member findMember = memberService.findMember(1L);
+            System.out.println("new Member = " + member.getName());
+            System.out.println("find Member = " + findMember.getName());
+        }
+    }
 }
---
+++ src/main/java/com/hello/core/order/OrderApp.java
@@ -0,0 +1,21 @@
+package com.hello.core.order;
+
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+
+public class OrderApp
+{
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+        
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order =" + order);
+    }
+}
