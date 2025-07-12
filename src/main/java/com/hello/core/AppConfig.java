3ffc1903ad98199658e4fe13eed0c889aa88166c
mkyu0917 <mkyu0917@gmail.com>
2025-07-12 15:35:37 +0900

    modified injection

--- src/main/java/com/hello/core/AppConfig.java
+++ src/main/java/com/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.hello.core;
 
+import com.hello.core.discount.DiscountPolicy;
 import com.hello.core.discount.FixDiscountPolicy;
+import com.hello.core.discount.RateDiscountPolicy;
 import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemberService;
 import com.hello.core.member.MemberServiceImpl;
@@ -10,12 +12,22 @@ import com.hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberReposiotry();
+    }
+
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberReposiotry());
+        return new MemberServiceImpl(memberRepository());
 
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberReposiotry(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    public DiscountPolicy discountPolicy(){
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
 }
